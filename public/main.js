7a06febb4fac182acbf018b4a4c510457db3c295
DavidHaas <cavoom@me.com>
2020-04-09 12:26:30 -0400

    from youtube does not work

--- public/main.js
+++ public/main.js
@@ -1,29 +1,95 @@
 const form = document.getElementById('vote-form');
 
-form.addEventListener('submit', e => {
-    const choice = document.querySelector('input[name=os]:checked').value;
-    const data = {os: choice};
+form.addEventListener('submit', (e) => {    
 
-    fetch('http://localhost:3000/poll', {
+    const choise = document.querySelector('input[name=os]:checked').value;
+    
+    const data = {
+        os: choise
+    }
+    // method post
+    fetch('http://localhost:3000/polling', {
         method: 'post',
-        body: JSON.stringify(data),
-        header: new Headers({
+        body: JSON.stringify(data), 
+        headers: new Headers({
             'Content-Type': 'application/json'
         })
     })
     .then(res => res.json())
     .then(data => console.log(data))
-    .catch(err=> console.log(err));
+    .catch(e => console.log(e));
 
     e.preventDefault();
 });
+// method get
+fetch('http://localhost:3000/poll')
+    // body: JSON.stringify(data),
+    // headers: new Headers({
+    //     'Content-type': 'application/json'
+    // })
+    .then(res => res.json())
+    .then(data => {
+        const votes = data.votes;
+        const totalVotes = votes.length;
 
-// let dataPoints = [
-//     {label: 'Windows', y: 0},
-//     {label: 'MacOS', y: 0},
-//     {label: 'Linux', y: 0},
-//     {label: 'Ubunto', y: 0},
+        //count vote points - acc / current votes
+        const voteCounts = votes.reduce(
+            (acc, vote) => (
+                    (acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc
+                 ),
+                 {}
+            );
 
-// ]
+        // data poinst 
+        let dataPoints = [
+            { label: 'Windows', y: voteCounts.Windows}, 
+            { label: 'MacOs', y: voteCounts.MacOs}, 
+            { label: 'Linux', y: voteCounts.Linux}, 
+            { label: 'Other', y: voteCounts.Other}, 
+        ];
 
-//const chartContainer = document.querySelector()
+        const chartContainer = document.querySelector('#chartContainer');
+        if(chartContainer) {
+            var  chart =  new  CanvasJS.Chart('chartContainer', {
+                animationEnabled: true,
+                theme: 'theme1',
+                title: {
+                    text: `Total Votes: ${totalVotes}`, //enl
+                    fontWeight: "bolder",
+                    fontColor: "#008B8B",
+                    fontfamily: "tahoma",
+                    fontSize: 25,
+                    padding: "10"
+                },
+                data: [
+                    {
+                        type: 'column',
+                        dataPoints: dataPoints
+                    }
+                ]
+            });
+            chart.render();
+
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
+
+            var pusher = new Pusher('549c2bdbf09152653457', {
+            cluster: 'us2',
+            forceTLS: true
+            });
+
+            var channel = pusher.subscribe('os-poll');
+            channel.bind('os-vote', function(data) {
+                dataPoints = dataPoints.map(x => {
+                    if(x.label == data.os) {
+                        x.y += data.points;
+                        return x;
+                    } else {
+                        return x;
+                    }
+                });
+                chart.render();
+            });
+        }
+    })
+    .catch(e => console.log(e));
